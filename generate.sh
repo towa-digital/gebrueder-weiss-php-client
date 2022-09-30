3f6e9dfaf5d541083ea58bc62f983e5548b1b868
Martin Welte <martin.welte@towa.at>
2022-09-30 17:41:43 +0200

    feat: add version restrictions as recommended

--- generate.sh
+++ generate.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-npx @openapitools/openapi-generator-cli version-manager set 5.4.0
 npx @openapitools/openapi-generator-cli generate -g php -o . -i api-specification.yaml --package-name=towa/gbw-sdk -c additionalProps.json
 rm -f .travis.yml git_push.sh openapitools.json .php_cs
