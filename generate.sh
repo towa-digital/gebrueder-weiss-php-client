6d8f0ce25acb77a87985fed928727eefb1f05fd8
Martin Welte <martin.welte@towa.at>
2022-09-30 17:22:13 +0200

    fix: use node openapi generator version

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-openapi-generator generate -g php -o . -i api-specification.yaml --package-name=towa/gbw-sdk -c additionalProps.json
+npx @openapitools/openapi-generator-cli version-manager set 5.4.0
+npx @openapitools/openapi-generator-cli generate -g php -o . -i api-specification.yaml --package-name=towa/gbw-sdk -c additionalProps.json
 rm -f .travis.yml git_push.sh openapitools.json .php_cs
