6d9b16d419e7276fd549082feb6d339be99b1f6e
Martin Welte <martin.welte@towa.at>
2022-09-30 17:43:43 +0200

    fix: reset unwanted files in generate sh script

--- generate.sh
+++ generate.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 npx @openapitools/openapi-generator-cli generate -g php -o . -i api-specification.yaml --package-name=towa/gbw-sdk -c additionalProps.json
-rm -f .travis.yml git_push.sh openapitools.json .php_cs
+rm -f .travis.yml git_push.sh
+
+# resetting unwanted files
+git checkout -- .gitignore
+git checkout -- composer.json
+git checkout -- .php_cs
