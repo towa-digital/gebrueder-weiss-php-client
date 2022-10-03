ba965d4c8cfa0d3ae50218929c07e8417378b617
Martin Welte <martin.welte@towa.at>
2022-10-03 09:57:46 +0200

    feat: add template for generation

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npx @openapitools/openapi-generator-cli generate -g php -o . -i api-specification.yaml --package-name=towa/gbw-sdk -c additionalProps.json
+npx @openapitools/openapi-generator-cli generate -t custom_template -g php -o  . -i api-specification.yaml --package-name=towa/gbw-sdk -c additionalProps.json
 rm -f .travis.yml git_push.sh
 
 # resetting unwanted files
